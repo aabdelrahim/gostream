8255711c7f29349133e5b169c4fb98084d672f88
Akram Abdelrahim <zapemz@gmail.com>
2019-02-20 16:54:41 -0600

    add simple grpc server

---
+++ backend/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/aabdelrahim/gostream/api"
+	"github.com/aabdelrahim/gostream/backend/song"
+	grpc "google.golang.org/grpc"
+)
+
+func main() {
+	port := flag.Int("p", 8080, "port to listen to")
+	flag.Parse()
+
+	s := grpc.NewServer()
+
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("could not listen to port %d: %v", *port, err)
+	}
+
+	pb.RegisterSongServiceServer(s, song.SongService{})
+
+	err = s.Serve(listener)
+	if err != nil {
+		log.Fatalf("could not serve %v", err)
+	}
+}
