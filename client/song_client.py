414d09e03f9ad07ee8e9a7f1ee677f85af61e902
Akram Abdelrahim <zapemz@gmail.com>
2019-02-23 17:38:03 -0600

    add update endpoint test

--- client/song_client.py
+++ client/song_client.py
@@ -5,6 +5,16 @@ import logging
 
 
 
+def add_song(stub):
+    Song = songservice_pb2.Song(name="SName", artists=["A", "B"], audio=None)
+    Request = songservice_pb2.AddSongRequest(newSong=Song)
+    return stub.Add(Request)
+
+def update_song(stub):
+    Song = songservice_pb2.Song(name="SName", artists=["A", "B"], audio=None)
+    Request = songservice_pb2.UpdateSongRequest(songID="ID-4321", updatedSong=Song)
+    return stub.Update(Request)
+
 def delete_song(stub):
     return stub.Delete(songservice_pb2.DeleteSongRequest(SongID="ID-1234"))
 
@@ -15,8 +25,12 @@ def run():
     # of the code.
     with grpc.insecure_channel('localhost:8080') as channel:
         stub = songservice_pb2_grpc.SongServiceStub(channel)
-        print("-------------- Delete Song --------------")
-        delete_song(stub)
+        print("-------------- Add Song --------------")
+        add_song(stub)
+        print("-------------- Update Song --------------")
+        update_song(stub)
+        # print("-------------- Delete Song --------------")
+        # delete_song(stub)
 
 
 if __name__ == '__main__':
