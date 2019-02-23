f87a056616ea2b25585cd43ad6840e19dd65a240
Akram Abdelrahim <zapemz@gmail.com>
2019-02-23 17:37:47 -0600

    implement stubs of api validators

--- backend/song/song.go
+++ backend/song/song.go
@@ -52,7 +52,7 @@ func (s Service) Add(ctx context.Context, req *pb.AddSongRequest) (*pb.Empty, er
 		fmt.Printf("Request was empty\n")
 		return &pb.Empty{}, nil
 	}
-	fmt.Printf(">>>>>Add Song Request Alert<<<<<\n")
+	fmt.Printf(">>>>> Add Song Request Alert <<<<<\n")
 
 	newSong := req.GetNewSong()
 
@@ -73,18 +73,64 @@ func (s Service) Add(ctx context.Context, req *pb.AddSongRequest) (*pb.Empty, er
 // Get is the API validation method for incoming rpc requests
 func (s Service) Get(ctx context.Context, req *pb.GetSongRequest) (*pb.GetSongResponse, error) {
 	fmt.Printf("Getting song with request: %v\n", &req)
+	if req == nil {
+		fmt.Printf("Request was empty\n")
+		return nil, nil
+	}
+
+	fmt.Printf(">>>>> Add Song Request Alert <<<<<\n")
+
+	request := &GetSongRequest{
+		Name:    req.GetName(),
+		Artists: req.GetArtists(),
+		SongID:  req.GetSongID(),
+	}
+
+	fmt.Printf("Getting song with request: %v\n", request)
+
 	return &pb.GetSongResponse{}, nil
 }
 
 // Update is the API validation method for incoming rpc requests
 func (s Service) Update(ctx context.Context, req *pb.UpdateSongRequest) (*pb.Empty, error) {
-	fmt.Printf("Updating song with request: %v\n", &req)
+	if req == nil {
+		fmt.Printf("Request was empty\n")
+		return &pb.Empty{}, nil
+	}
+	fmt.Printf(">>>>> Update Song Request Alert <<<<<\n")
+
+	updatedSong := req.GetUpdatedSong()
+
+	fmt.Printf("New Song Request: %v\n", &updatedSong)
+
+	request := &UpdateSongRequest{
+		SongID: req.GetSongID(),
+		UpdatedSong: &Song{
+			Name:    updatedSong.GetName(),
+			Artists: updatedSong.GetArtists(),
+			Audio:   updatedSong.GetAudio(),
+		},
+	}
+
+	fmt.Printf("Updating song with request: %v\n", request)
+
 	return &pb.Empty{}, nil
 }
 
 // Delete is the API validation method for incoming rpc requests
 func (s Service) Delete(ctx context.Context, req *pb.DeleteSongRequest) (*pb.Empty, error) {
-	fmt.Printf("Deleting song with request: %v\n", &req)
+	if req == nil {
+		fmt.Printf("Request was empty\n")
+		return &pb.Empty{}, nil
+	}
+	fmt.Printf(">>>>> Delete Song Request Alert <<<<<\n")
+
+	request := &DeleteSongRequest{
+		SongID: req.GetSongID(),
+	}
+
+	fmt.Printf("Deleting song with request: %v\n", request)
+
 	return &pb.Empty{}, nil
 }
 
