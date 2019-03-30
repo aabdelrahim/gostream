5867f1672e2fbfb0df10bc9586243f119e683784
Akram Abdelrahim <zapemz@gmail.com>
2019-03-30 17:59:14 -0600

    add metadata file and filder

--- backend/song/repo.go
+++ backend/song/repo.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 // CreateRepo creates a new instance of Repo ready to be used
@@ -17,7 +18,15 @@ type Repo struct{}
 // Add creates a new audio file for a given song
 func (r Repo) Add(ctx context.Context, song *Song) error {
 	fmt.Printf(">>> Add Repo Method called <<<\n\n")
-	err := ioutil.WriteFile("./"+song.Name+"."+song.AudioFormat, song.Audio, 0644)
+	path := "./musicLibrary/" + song.Name + "." + song.AudioFormat
+	err := ioutil.WriteFile(path, song.Audio, 0644)
+
+	f, err := os.Create("./musicLibrary/songmetadata")
+	check(err)
+	defer f.Close()
+
+	f.Write([]byte(stringifySong(song, path)))
+
 	check(err)
 	return nil
 }
@@ -26,4 +35,15 @@ func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func stringifySong(song *Song, path string) string {
+	resp := song.SongID
+	resp += "- " + song.Name + "\n"
+	resp += " path: " + path + "\n"
+	for _, v := range song.Artists {
+		resp += " Artist: " + v + "\n"
+	}
+
+	return resp
 }
