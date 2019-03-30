3b31c9aedf05456cc2356e345907e12a715720a7
Akram Abdelrahim <zapemz@gmail.com>
2019-03-30 17:29:49 -0600

    clean up prints to be more readable

--- backend/song/service.go
+++ backend/song/service.go
@@ -18,25 +18,25 @@ type Service struct {
 
 // Add is the service method for handling domain logic
 func (s Service) Add(ctx context.Context, req *AddSongRequest) error {
-	fmt.Printf(">>> Add Service Method called <<<\n\n")
+	fmt.Printf(">>> Add Service Method called <<<\n")
 	err := s.repo.Add(ctx, req.Song)
 	return err
 }
 
 // Get is the service method for handling domain logic
 func (s Service) Get(ctx context.Context, req *GetSongRequest) (*GetSongResponse, error) {
-	fmt.Printf(">>> Get Service Method called <<<\n\n")
+	fmt.Printf(">>> Get Service Method called <<<\n")
 	return nil, nil
 }
 
 // Update is the service method for handling domain logic
 func (s Service) Update(ctx context.Context, req *UpdateSongRequest) error {
-	fmt.Printf(">>> Update Service Method called <<<\n\n")
+	fmt.Printf(">>> Update Service Method called <<<\n")
 	return nil
 }
 
 // Delete is the service method for handling domain logic
 func (s Service) Delete(ctx context.Context, req *DeleteSongRequest) error {
-	fmt.Printf(">>> Delete Service Method called <<<\n\n")
+	fmt.Printf(">>> Delete Service Method called <<<\n")
 	return nil
 }
